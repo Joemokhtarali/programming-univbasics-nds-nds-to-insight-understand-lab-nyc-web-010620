1bbfd5e1272703af3a1fb097be1bc82930b0c81b
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-30 04:37:26 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,10 +10,10 @@ def pretty_print_nds(nds)
 end
    
 def print_first_directors_movie_titles
-    
+    array = []
   movies = directors_database[0][:movies]
     movies.each do |movie|
-      movie[:title]
+      array << movie[:title]
     
   end 
 end 
