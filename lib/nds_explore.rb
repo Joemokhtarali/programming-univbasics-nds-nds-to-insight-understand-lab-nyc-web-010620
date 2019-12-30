ca5d41473f0114f2532d88f930850f506b7d881b
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-30 04:33:57 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ def pretty_print_nds(nds)
  pp nds
 end
    
-def print_first_directors_movie_titles
+def print_first_directors_movie_titles(data)
     binding.pry
   movies = directors_database[0][:movies]
     movies.each do |movie|
