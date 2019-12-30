51c04b62f5a83a404a5665e9a0e885e220a26b64
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-30 04:33:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,9 +8,10 @@ require 'directors_database'
 def pretty_print_nds(nds)
  pp nds
 end
-     binding.pry
+   
 def print_first_directors_movie_titles
   titles = []
+    binding.pry
   movies = directors_database[0][:movies]
     movies.each do |movie|
  
